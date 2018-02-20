ea7fa563a10c27afb3d7ecd491dfc8349312bef9
RomainEl <34889902+RomainEl@users.noreply.github.com>
2018-02-20 16:33:36 +0100

    fin projet annonceo

--- admin/stats.php
+++ admin/stats.php
@@ -4,24 +4,59 @@ require_once('../inc/init.php');
 
 //calcul moyennes
 
-$somme_notes = 0;
-$i = 0;
-foreach($note as $cle=>$valeur)
-{
-    $i++;
-    $somme_notes+=$valeur;// équivaut a $somme_notes = $somme_notes + $valeur
-}
-$moyenne = $somme_notes / $i;
+$result = $pdo->prepare("SELECT m.pseudo,n.membre_id1,AVG(n.note),COUNT(n.note) FROM note n,membre m  WHERE m.id_membre=n.membre_id1 GROUP BY membre_id1 ORDER BY AVG(n.note) DESC LIMIT 5");
 
-$contenu .= '<h2>TOP 5 des membres les mieux notés</h2>
+$result->execute(array());
+
+$contenu .= '<h2>Top 5 des utilisateurs les mieux notés</h2>
                 <ul class="list-group">';
-$a = 1;
-while($a <= 5)
-{
-    $contenu .= '<li class="list-group-item">'.$a.'-'..'<span class="badge">'.$moyenne.' étoiles basé sur '.$i.' avis</span>';
-    $a++;
+
+while($moyenne= $result->fetch(PDO::FETCH_ASSOC)){
+    //var_dump($moyenne);
+    $contenu .= '
+                <li class="list-group-item">
+                    <span class="badge">'.$moyenne['AVG(n.note)'].' étoiles basé sur '.$moyenne['COUNT(n.note)'].' avis</span>'
+                    .$moyenne['pseudo'].'
+                </li>';
 }
+$contenu .= '</ul>';
 
+// catégories avec le plus d'annonces
 
+$result = $pdo->prepare("SELECT c.titre,COUNT(a.categorie_id) FROM categorie c,annonce a WHERE a.categorie_id=c.id_categorie ORDER BY COUNT(a.categorie_id) DESC");
+
+$result->execute(array());
+
+$contenu.= '<h2>Top 5 des catégories avec le plus d\'annonces<h2>
+                <ul class="list-group">';
+
+while($annonce=$result->fetch(PDO::FETCH_ASSOC))
+{
+    $contenu .=' <li class="list-group-item">
+                    <span class="badge">'.$annonce['COUNT(a.categorie_id)'].'</span>'
+                    .$annonce['titre'].'
+                </li>';
+}
+$contenu .= '</ul>';
+
+// membres les plus actifs
+
+$result = $pdo->prepare("SELECT m.pseudo,a.id_annonce,COUNT(a.membre_id) FROM membre m, annonce a WHERE a.membre_id=m.id_membre ORDER BY COUNT(a.membre_id) DESC");
+
+$result->execute(array());
+
+$contenu.= '<h2>Top 5 des membres les plus actifs<h2>
+                <ul class="list-group">';
+
+while($actif=$result->fetch(PDO::FETCH_ASSOC))
+{
+    $contenu .= '<li class="list-group-item">
+                    <span class="badge">'.$actif['COUNT(a.membre_id)'].'</span>'
+                    .$actif['pseudo'].'
+                </li>';
+}
+$contenu .= '</ul>';
 require_once('../inc/header.php');
+echo $contenu;
 
+require_once('../inc/footer.php');
--- profil.php
+++ profil.php
@@ -7,6 +7,49 @@ if(!estConnecte())
     header('location:connexion.php');
     exit;
 }
+
+
+if($_POST){
+
+    $result = $pdo->prepare("REPLACE INTO membre VALUES(:id_membre,:pseudo,:mdp,:nom,:prenom,:telephone,:email,:civilite,:statut,NOW()");
+    
+    $params = $_POST['mdp'];
+    if(!empty($params))
+    {
+        $result->execute(array('id_membre'=>$_POST['id_membre'],
+                            'pseudo'=>$_POST ['pseudo'],
+                            'mdp' =>$_POST['mdp'],
+                            'nom'=> $params,
+                            'prenom'=>$_POST['prenom'],
+                            'telephone'=>$_POST['telephone'],
+                            'email'=>$_POST['email'],
+                            'civilite'=>$_POST['civilite'],
+                            'statut'=>$_POST['statut']));
+    }
+    else{
+        $result->execute(array('id_membre'=>$_POST['id_membre'],
+                            'pseudo'=>$_POST ['pseudo'],
+                            'mdp' =>$_POST['mdp'],
+                            'nom'=> $_SESSION['membre']['mdp'],
+                            'prenom'=>$_POST['prenom'],
+                            'telephone'=>$_POST['telephone'],
+                            'email'=>$_POST['email'],
+                            'civilite'=>$_POST['civilite'],
+                            'statut'=>$_POST['statut']));
+    }
+    
+
+    $contenu.= '<div class="alert alert-success">Vos informations ont été modifiées</div>
+    <div><h3>Vos informations de profil</h3>
+    <p>Pseudo : '.$_POST['pseudo'].'</p>
+    <p>Email : '.$_POST['email'].'</p>
+    <p>Nom, Prénom : '.$_POST['nom'].', '.$_POST['prenom'].'</p>
+    <p>Téléphone: '.$_POST['telephone'].'</p>
+    <p>Civilité: '.$_POST['civilite'].'</p></div>
+    <a href="?action=modifier" class="btn btn-default" type="submit">Modifier vos informations</a>';                         
+
+}
+
 if (!isset($_GET['action']) || $_GET['action'] != 'modifier')
 {
     $contenu .= '<h2>Bonjour '.ucfirst($_SESSION['membre']['pseudo']).'</h2>';
@@ -16,13 +59,16 @@ if (!isset($_GET['action']) || $_GET['action'] != 'modifier')
         $contenu.= '<p>Vous êtes connecté en tant qu\'administrateur</p>';
 
         $contenu.= '<div><h3>Vos informations de profil</h3>
+            <p>Pseudo : '.$_SESSION['membre']['pseudo'].'</p>
             <p>Email : '.$_SESSION['membre']['email'].'</p>
             <p>Nom, Prénom : '.$_SESSION['membre']['nom'].', '.$_SESSION['membre']['prenom'].'</p>
             <p>Téléphone: '.$_SESSION['membre']['telephone'].'</p>
-            <p>Civilité: '.$_SESSION['membre']['civilite'].'</p></div>';
+            <p>Civilité: '.$_SESSION['membre']['civilite'].'</p></div>
+            <a href="?action=modifier" class="btn btn-default" type="submit">Modifier vos informations</a>';
     }
     else{
         $contenu.= '<div><h3>Vos informations de profil</h3>
+        <p>Pseudo : '.$_SESSION['membre']['pseudo'].'</p>
         <p>Email : '.$_SESSION['membre']['email'].'</p>
         <p>Nom, Prénom : '.$_SESSION['membre']['nom'].', '.$_SESSION['membre']['prenom'].'</p>
         <p>Téléphone: '.$_SESSION['membre']['telephone'].'</p>
@@ -30,51 +76,62 @@ if (!isset($_GET['action']) || $_GET['action'] != 'modifier')
         <a href="?action=modifier" class="btn btn-default" type="submit">Modifier vos informations</a>';
     }
 }
+
+
 require_once('inc/header.php');
 echo $contenu;
-if ($_GET['action'] == 'modifier'){
+if (isset($_GET['action']) && $_GET['action'] == 'modifier'){
+    if(!$_POST){
 ?>
 
 <h2>Modifier vos données</h2>
 <form novalidate class="form-horizontal" method="post" action="">
     <div class="form-group">
+        <input type="hidden" name="id_membre" id="id_membre" value="<?= $_SESSION['membre']['id_membre'] ?? '' ?>">
+    </div>
+    <div class="form-group">
         <label for="pseudo">Pseudo</label>
-        <input type="text" name="pseudo" class="form-control" id="pseudo" placeholder="Pseudo" value="<?= $_POST['pseudo'] ?? '' ?>">
+        <input type="text" name="pseudo" class="form-control" id="pseudo" placeholder="Pseudo" value="<?= $_SESSION['membre']['pseudo'] ?? '' ?>">
     </div>
     <div class="form-group">
         <label for="mdp">Password</label>
-        <input type="password" name="mdp" class="form-control" id="mdp" placeholder="Password" value="<?= $_POST['password'] ?? '' ?>">
+        <input type="password" name="mdp" class="form-control" id="mdp" placeholder="Password" value="<?= $_SESSION['membre']['mdp'] ?? '' ?>">
     </div>
     <div class="form-group">
         <label for="nom">Nom</label>
-        <input type="text" name="nom" id="nom" class="form-control" placeholder="Nom" value="<?= $_POST['nom'] ?? '' ?>">
+        <input type="text" name="nom" id="nom" class="form-control" placeholder="Nom" value="<?= $_SESSION['membre']['nom'] ?? '' ?>">
     </div>
     <div class="form-group">
         <label for="prenom">Prenom</label>
-        <input type="text" name="prenom" id="prenom" class="form-control" placeholder="Prenom" value="<?= $_POST['prenom'] ?? '' ?>">
+        <input type="text" name="prenom" id="prenom" class="form-control" placeholder="Prenom" value="<?= $_SESSION['membre']['prenom'] ?? '' ?>">
     </div>
     <div class="form-group">
         <label for="telephone">Téléphone</label>
-        <input type="text" name="telephone" id="telephone" class="form-control" placeholder="Téléphone" value="<?= $_POST['telephone'] ?? '' ?>">
+        <input type="text" name="telephone" id="telephone" class="form-control" placeholder="Téléphone" value="<?= $_SESSION['membre']['telephone'] ?? '' ?>">
     </div>
     <div class="form-group">
         <div class="col-sm-1">
             <label for="nom">Civilité</label>
         </div>
         <div class="col-sm-4">
-            <input type="radio" name="civilite" class="form-control" value="f" <?=((isset($_POST['civilite']) 
-               && $_POST['civilite'] == 'f')) ? 'checked' : ''?>>Femme
-            <input type="radio" name="civilite" class="form-control" value="m" <?=((isset($_POST['civilite']) 
-            && $_POST['civilite'] == 'm')) || !isset($_POST['civilite']) ? 'checked' : ''?>>Homme
+            <input type="radio" name="civilite" class="form-control" value="f" <?=((isset($_SESSION['membre']['civilite']) 
+               && $_SESSION['membre']['civilite'] == 'f')) ? 'checked' : ''?>>Femme
+            <input type="radio" name="civilite" class="form-control" value="m" <?=((isset($_SESSION['membre']['civilite']) 
+            && $_SESSION['membre']['civilite'] == 'm')) || !isset($_SESSION['membre']['civilite']) ? 'checked' : ''?>>Homme
         </div>
     </div>
     <div class="form-group">
         <label for="email">Email</label>
-        <input type="text" name="email" id="email" class="form-control" placeholder="Email" value="<?= $_POST['email'] ?? '' ?>">
+        <input type="text" name="email" id="email" class="form-control" placeholder="Email" value="<?= $_SESSION['membre']['email'] ?? '' ?>">
     </div>
+    <div class="form-group">
+        <input type="hidden" name="statut" id="statut" value="<?= $_SESSION['membre']['statut'] ?? '' ?>">
+    </div>
+
     <button type="submit" class="btn btn-default">Valider</button>
 </form>
 <?php
+    }
 }
 require_once('inc/footer.php');
 ?>
